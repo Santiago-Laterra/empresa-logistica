9da805f8e2265aad4473e9e4f09bde1df0c1f0b2
Santiago Laterra <santiagolaterra05@gmail.com>
2026-03-25 14:06:53 -0300

    schema del usuario hecho

---
+++ src/model/userModel.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+import { IUser } from "../interfaces/IUser";
+
+
+
+const userSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true }, // Recordá usar bcrypt para el hash
+  role: {
+    type: String,
+    enum: ['super_admin', 'operator', 'client'],
+    default: 'client'
+  },
+  phone: { type: String }, // Útil para logística
+  active: { type: Boolean, default: true } // Para "dar de baja" usuarios sin borrarlos
+}, { timestamps: true });
+
+export default userSchema
