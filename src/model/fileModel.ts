c5c81e1efc97f44f1df1f28456f80c1fdcb24466
Santiago Laterra <santiagolaterra05@gmail.com>
2026-03-25 14:20:19 -0300

    estructura del modelo del archivo

---
+++ src/model/fileModel.ts
@@ -0,0 +1,30 @@
+import { Schema, Types } from "mongoose";
+import IFile from "../interfaces/IFile";
+
+
+const fileSchema = new Schema<IFile>({
+  title: { type: String, required: true }, // Nombre que verá el cliente
+  fileName: { type: String, required: true }, // Nombre original del archivo
+  fileUrl: { type: String, required: true }, // La URL que te dará AWS o Cloudinary
+  fileType: { type: String }, // 'pdf', 'xlsx', etc.
+
+  // Relaciones
+  receiverId: {
+    type: Types.ObjectId,
+    ref: 'User',
+    required: true
+  }, // El Cliente que lo recibe
+  uploaderId: {
+    type: Types.ObjectId,
+    ref: 'User',
+    required: true
+  }, // El Admin/Operador que lo subió
+
+  status: {
+    type: String,
+    enum: ['active', 'archived', 'deleted'],
+    default: 'active'
+  }
+}, { timestamps: true });
+
+export default fileSchema
